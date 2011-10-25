626752b5ad60b2bee466d48d0817d49b39a85f96
Michiel de Jong <michiel@unhosted.org>
2011-10-25 10:48:36 +0000

    clean up IOU.js a bit

--- domains/opentabs.net/IOU.js
+++ domains/opentabs.net/IOU.js
@@ -1,283 +1,31 @@
-var sampleData = localStorage.getItem('tabs');
-if(sampleData==null) sampleData = [
-  {
-    payer: 'you',
-    payee: 'mich@unhosted.org',
-    amount: 1,
-    currency: 'beer',
-    timestamp: 1234567890.123,
-    proposer: 'you',
-    status: 'proposed'
-  },
-  {
-    payer: 'mich@unhosted.org',
-    payee: 'you',
-    amount: 2,
-    currency: 'beer',
-    timestamp: 1234567890.123,
-    proposer: 'you',
-    status: 'proposed'
-  },
-  {
-    payer: 'you',
-    payee: 'mich@unhosted.org',
-    amount: 3,
-    currency: 'beer',
-    timestamp: 1234567890.123,
-    proposer: 'mich@unhosted.org',
-    status: 'proposed'
-  },
-  {
-    payer: 'mich@unhosted.org',
-    payee: 'you',
-    amount: 4,
-    currency: 'beer',
-    timestamp: 1234567890.123,
-    proposer: 'mich@unhosted.org',
-    status: 'proposed'
-  },
-  {
-    payer: 'you',
-    payee: 'mich@unhosted.org',
-    amount: 11,
-    currency: 'beer',
-    timestamp: 1234567890.123,
-    proposer: 'you',
-    status: 'declined'
-  },
-  {
-    payer: 'mich@unhosted.org',
-    payee: 'you',
-    amount: 12,
-    currency: 'beer',
-    timestamp: 1234567890.123,
-    proposer: 'you',
-    status: 'declined'
-  },
-  {
-    payer: 'you',
-    payee: 'mich@unhosted.org',
-    amount: 13,
-    currency: 'beer',
-    timestamp: 1234567890.123,
-    proposer: 'mich@unhosted.org',
-    status: 'declined'
-  },
-  {
-    payer: 'mich@unhosted.org',
-    payee: 'you',
-    amount: 14,
-    currency: 'beer',
-    timestamp: 1234567890.123,
-    proposer: 'mich@unhosted.org',
-    status: 'declined'
-  },
-  {
-    payer: 'you',
-    payee: 'mich@unhosted.org',
-    amount: 21,
-    currency: 'beer',
-    timestamp: 1234567890.123,
-    proposer: 'you',
-    status: 'accepted'
-  },
-  {
-    payer: 'mich@unhosted.org',
-    payee: 'you',
-    amount: 22,
-    currency: 'beer',
-    timestamp: 1234567890.123,
-    proposer: 'you',
-    status: 'accepted'
-  },
-  {
-    payer: 'you',
-    payee: 'mich@unhosted.org',
-    amount: 23,
-    currency: 'beer',
-    timestamp: 1234567890.123,
-    proposer: 'mich@unhosted.org',
-    status: 'accepted'
-  },
-  {
-    payer: 'mich@unhosted.org',
-    payee: 'you',
-    amount: 24,
-    currency: 'beer',
-    timestamp: 1234567890.123,
-    proposer: 'mich@unhosted.org',
-    status: 'accepted'
-  },
-  {
-    payer: 'you',
-    payee: 'mich@unhosted.org',
-    amount: 31,
-    currency: 'beer',
-    timestamp: 1234567890.123,
-    proposer: 'you',
-    status: 'requested'
-  },
-  {
-    payer: 'mich@unhosted.org',
-    payee: 'you',
-    amount: 32,
-    currency: 'beer',
-    timestamp: 1234567890.123,
-    proposer: 'you',
-    status: 'requested'
-  },
-  {
-    payer: 'you',
-    payee: 'mich@unhosted.org',
-    amount: 33,
-    currency: 'beer',
-    timestamp: 1234567890.123,
-    proposer: 'mich@unhosted.org',
-    status: 'requested'
-  },
-  {
-    payer: 'mich@unhosted.org',
-    payee: 'you',
-    amount: 34,
-    currency: 'beer',
-    timestamp: 1234567890.123,
-    proposer: 'mich@unhosted.org',
-    status: 'requested'
-  },
-  {
-    payer: 'you',
-    payee: 'mich@unhosted.org',
-    amount: 41,
-    currency: 'beer',
-    timestamp: 1234567890.123,
-    proposer: 'you',
-    status: 'sent'
-  },
-  {
-    payer: 'mich@unhosted.org',
-    payee: 'you',
-    amount: 42,
-    currency: 'beer',
-    timestamp: 1234567890.123,
-    proposer: 'you',
-    status: 'sent'
-  },
-  {
-    payer: 'you',
-    payee: 'mich@unhosted.org',
-    amount: 43,
-    currency: 'beer',
-    timestamp: 1234567890.123,
-    proposer: 'mich@unhosted.org',
-    status: 'sent'
-  },
-  {
-    payer: 'mich@unhosted.org',
-    payee: 'you',
-    amount: 44,
-    currency: 'beer',
-    timestamp: 1234567890.123,
-    proposer: 'mich@unhosted.org',
-    status: 'sent'
-  },
-  {
-    payer: 'you',
-    payee: 'mich@unhosted.org',
-    amount: 51,
-    currency: 'beer',
-    timestamp: 1234567890.123,
-    proposer: 'you',
-    status: 'received'
-  },
-  {
-    payer: 'mich@unhosted.org',
-    payee: 'you',
-    amount: 52,
-    currency: 'beer',
-    timestamp: 1234567890.123,
-    proposer: 'you',
-    status: 'received'
-  },
-  {
-    payer: 'you',
-    payee: 'mich@unhosted.org',
-    amount: 53,
-    currency: 'beer',
-    timestamp: 1234567890.123,
-    proposer: 'mich@unhosted.org',
-    status: 'received'
-  },
-  {
-    payer: 'mich@unhosted.org',
-    payee: 'you',
-    amount: 54,
-    currency: 'beer',
-    timestamp: 1234567890.123,
-    proposer: 'mich@unhosted.org',
-    status: 'received'
-  },
-  {
-    payer: 'you',
-    payee: 'mich@unhosted.org',
-    amount: 61,
-    currency: 'beer',
-    timestamp: 1234567890.123,
-    proposer: 'you',
-    status: 'closed'
-  },
-  {
-    payer: 'mich@unhosted.org',
-    payee: 'you',
-    amount: 62,
-    currency: 'beer',
-    timestamp: 1234567890.123,
-    proposer: 'you',
-    status: 'closed'
-  },
-  {
-    payer: 'you',
-    payee: 'mich@unhosted.org',
-    amount: 63,
-    currency: 'beer',
-    timestamp: 1234567890.123,
-    proposer: 'mich@unhosted.org',
-    status: 'closed'
-  },
-  {
-    payer: 'mich@unhosted.org',
-    payee: 'you',
-    amount: 64,
-    currency: 'beer',
-    timestamp: 1234567890.123,
-    proposer: 'mich@unhosted.org',
-    status: 'closed'
-  }
-  ];
-
+var tabs = localStorage.getItem('tabs');
+var you = 'michiel@unhosted.org';
 var peers = {};
-for(i in sampleData) {
-  peers[sampleData[i].payer]=true;
-  peers[sampleData[i].payee]=true;
+for(i in tabs) {
+  peers[tabs[i].payer]=true;
+  peers[tabs[i].payee]=true;
 }
-delete peers['you'];
+delete peers[you];
 
-document.write('<h2>IMPORTANT</h2><ul>');
+var importantStr = '<h2>IMPORTANT</h2><ul>';
 for(i in sampleData) {
   var iou = sampleData[i];
   if((iou.proposer != 'you') && (iou.status == 'proposed') && (iou.payer == 'you')) {
-    document.write('<li style="background-color:yellow">[incoming invoice:] [?]'+iou.payee+' '+iou.amount+iou.currency+'</li>');
+    importantStr += '<li style="background-color:yellow">[incoming invoice:] [?]'+iou.payee+' '+iou.amount+iou.currency+'</li>';
   }
   if((iou.proposer != 'you') && (iou.status == 'proposed') && (iou.payee == 'you')) {
-    document.write('<li style="background-color:yellow">[incoming IOU:] [?]'+iou.payer+' '+iou.amount+iou.currency+'</li>');
+    importantStr += '<li style="background-color:yellow">[incoming IOU:] [?]'+iou.payer+' '+iou.amount+iou.currency+'</li>';
   }
   if((iou.proposer == 'you') && (iou.status == 'declined') && (iou.payer == 'you')) {
-    document.write('<li style="background-color:yellow">[declined your IOU:] [X]'+iou.payee+' '+iou.amount+iou.currency+'</li>');
+    importantStr += '<li style="background-color:yellow">[declined your IOU:] [X]'+iou.payee+' '+iou.amount+iou.currency+'</li>';
   }
   if((iou.proposer == 'you') && (iou.status == 'declined') && (iou.payee == 'you')) {
-    document.write('<li style="background-color:yellow">[declined your invoice:] [X]'+iou.payer+' '+iou.amount+iou.currency+'</li>');
+    importantStr += '<li style="background-color:yellow">[declined your invoice:] [X]'+iou.payer+' '+iou.amount+iou.currency+'</li>';
   }
 }
-document.write('</ul><h2>Contacts:</h2><table id="contacts"></table>');
+importantStr += '</ul><h2>Contacts:</h2><table id="contacts"></table>';
+document.write(importantStr);
+
 
 for(var peer in peers) {
   document.write('<h2>'+peer+'</h2><ul>');
