d5450f515bba9159d89c323825b0fe96a9b236eb
Michiel de Jong <michiel@unhosted.org>
2011-10-26 13:57:11 +0000

    add more buttons

--- domains/opentabs.net/IOU.js
+++ domains/opentabs.net/IOU.js
@@ -12,21 +12,29 @@ function getImportantString() {
     }
     if((iou.proposer != you) && (iou.status == 'proposed') && (iou.payer == you)) {
       importantStr += '<li style="background-color:yellow">[incoming invoice:] [?]'
-        +iou.payee+' '
-        +iou.description+' '
-        +iou.amount+iou.currency
+        +iou.payee+' '+iou.description+' '+iou.amount+iou.currency
         +'<input type="submit" value="Decline" onclick="declineIncoming('+i+');">'
         +'<input type="submit" value="Accept" onclick="acceptIncoming('+i+');">'
         +'</li>';
     }
     if((iou.proposer != you) && (iou.status == 'proposed') && (iou.payee == you)) {
-      importantStr += '<li style="background-color:yellow">[incoming IOU:] [?]'+iou.payer+' '+iou.description+' '+iou.amount+iou.currency+'</li>';
+      importantStr += '<li style="background-color:yellow">[incoming IOU:] [?]'
+        +iou.payer+' '+iou.description+' '+iou.amount+iou.currency
+        +'<input type="submit" value="Decline" onclick="declineIncoming('+i+');">'
+        +'<input type="submit" value="Accept" onclick="acceptIncoming('+i+');">'
+        +'</li>';
     }
     if((iou.proposer == you) && (iou.status == 'declined') && (iou.payer == you)) {
-      importantStr += '<li style="background-color:yellow">[declined your IOU:] [X]'+iou.payee+' '+iou.description+' '+iou.amount+iou.currency+'</li>';
+      importantStr += '<li style="background-color:yellow">[declined your IOU:] [X]'
+        +iou.payee+' '+iou.description+' '+iou.amount+iou.currency
+        +'<input type="submit" value="Close" onclick="closeDeclined('+i+');">'
+        +'</li>';
     }
     if((iou.proposer == you) && (iou.status == 'declined') && (iou.payee == you)) {
-      importantStr += '<li style="background-color:yellow">[declined your invoice:] [X]'+iou.payer+' '+iou.description+' '+iou.amount+iou.currency+'</li>';
+      importantStr += '<li style="background-color:yellow">[declined your invoice:] [X]'
+        +iou.payer+' '+iou.description+' '+iou.amount+iou.currency
+        +'<input type="submit" value="Close" onclick="closeDeclined('+i+');">'
+        +'</li>';
     }
   }
   importantStr += '</ul>';
@@ -47,13 +55,22 @@ function getContactsString() {
         iou.description='';
       }
       if((iou.payee == contacts[i]) && (iou.status == 'requested')) {
-        contactsStr += '<li style="background-color:pink">[hurry:] [!]'+iou.description+' '+iou.amount+iou.currency+'</li>';
+        contactsStr += '<li style="background-color:pink">[hurry:] [!]'
+          +iou.description+' '+iou.amount+iou.currency
+          +'<input type="submit" value="Mark as paid" onclick="markAsPaid('+i+');">'
+          +'</li>';
       }
       if((iou.payer == contacts[i]) && (iou.status == 'sent')) {
-        contactsStr += '<li style="background-color:green">[got it?] [&#10003;]'+iou.description+' '+iou.amount+iou.currency+'</li>';
+        contactsStr += '<li style="background-color:green">[got it?] [&#10003;]'
+          +iou.description+' '+iou.amount+iou.currency
+          +'<input type="submit" value="Mark as paid" onclick="markAsPaid('+i+');">'
+          +'</li>';
       }
       if((iou.payer == contacts[i]) && (iou.status == 'requested')) {
-        contactStr += '<li style="background-color:green">[you said hurry] [!]'+iou.description+' '+iou.amount+iou.currency+'</li>';
+        contactStr += '<li style="background-color:green">[you said hurry] [!]'
+          +iou.description+' '+iou.amount+iou.currency
+          +'<input type="submit" value="Mark as paid" onclick="markAsPaid('+i+');">'
+          +'</li>';
       }
     }
     contactsStr += '</ul><div onclick="unfold('+i+');">...<div id="folded'+i+'" style="display:none"><ul>';
@@ -63,13 +80,23 @@ function getContactsString() {
         iou.description='';
       }
       if((iou.payee == contacts[i]) && (iou.status == 'accepted')) {
-        contactsStr += '<li style="background-color:pink">[you owe them]'+iou.description+' '+iou.amount+iou.currency+'</li>';
+        contactsStr += '<li style="background-color:pink">[you owe them]'
+          +iou.description+' '+iou.amount+iou.currency
+          +'<input type="submit" value="Mark as paid" onclick="markAsPaid('+i+');">'
+          +'</li>';
       }
       if((iou.payer == contacts[i]) && (iou.status == 'accepted')) {
-        contactsStr += '<li style="background-color:green">[they owe you]'+iou.description+' '+iou.amount+iou.currency+'</li>';
+        contactsStr += '<li style="background-color:green">[they owe you]'
+          +iou.description+' '+iou.amount+iou.currency
+          +'<input type="submit" value="Request payment" onclick="requestPayment('+i+');">'
+          +'<input type="submit" value="Mark as paid" onclick="markAsPaid('+i+');">'
+          +'</li>';
       }
       if((iou.payee == contacts[i]) && (iou.status == 'proposed') && (iou.proposer == you)) {
-        contactsStr += '<li style="background-color:pink">[you proposed] [?]'+iou.description+' '+iou.amount+iou.currency+'</li>';
+        contactsStr += '<li style="background-color:pink">[you proposed] [?]'
+          +iou.description+' '+iou.amount+iou.currency
+          +'<input type="submit" value="Cancel" onclick="cancelProposed('+i+');">'
+          +'</li>';
       }
     }
     contactsStr += '</ul></div></div></div>';
@@ -86,25 +113,39 @@ function getHistoryString() {
       iou.description='';
     }
     if((iou.proposer != you) && (iou.status == 'declined') && (iou.payee==you)) {
-      historyStr += '<li style="background-color:yellow">[you declined] [X]'+iou.payer+' '+iou.description+' '+iou.amount+iou.currency+'</li>';
+      historyStr += '<li style="background-color:yellow">[offer you refused] [X]'
+        +iou.payer+' '+iou.description+' '+iou.amount+iou.currency
+        +'</li>';
     }
     if((iou.proposer != you) && (iou.status == 'declined') && (iou.payer==you)) {
-      historyStr += '<li style="background-color:yellow">[you declined] [X]'+iou.payee+' '+iou.description+' '+iou.amount+iou.currency+'</li>';
+      historyStr += '<li style="background-color:yellow">[invoice you declined] [X]'
+        +iou.payee+' '+iou.description+' '+iou.amount+iou.currency
+        +'</li>';
     }
     if((iou.payer == you) &&(iou.status == 'closed')) {
-      historyStr += '<li style="background-color:yellow">[was declined] [X]'+iou.payee+' '+iou.description+' '+iou.amount+iou.currency+'</li>';
+      historyStr += '<li style="background-color:yellow">[was declined] [X]'
+        +iou.payee+' '+iou.description+' '+iou.amount+iou.currency
+        +'</li>';
     }
     if((iou.payee == you) &&(iou.status == 'closed')) {
-      historyStr += '<li style="background-color:yellow">[was declined] [X]'+iou.payer+' '+iou.description+' '+iou.amount+iou.currency+'</li>';
+      historyStr += '<li style="background-color:yellow">[was declined] [X]'
+        +iou.payer+' '+iou.description+' '+iou.amount+iou.currency
+        +'</li>';
     }
     if((iou.payer == you) && (iou.status == 'sent')) {
-      historyStr += '<li style="background-color:pink">[you sent] [&#10003;]'+iou.payee+' '+iou.description+' '+iou.amount+iou.currency+'</li>';
+      historyStr += '<li style="background-color:pink">[you sent] [&#10003;]'
+        +iou.payee+' '+iou.description+' '+iou.amount+iou.currency
+        +'</li>';
     }
     if((iou.payer == you) && (iou.status == 'received')) {
-      historyStr += '<li style="background-color:pink">[they received] [&#10003;]'+iou.payee+' '+iou.description+' '+iou.amount+iou.currency+'</li>';
+      historyStr += '<li style="background-color:pink">[they received] [&#10003;]'
+        +iou.payee+' '+iou.description+' '+iou.amount+iou.currency
+        +'</li>';
     }
     if((iou.payee == you) && (iou.status == 'received')) {
-      historyStr += '<li style="background-color:green">[you received] [&#10003;]'+iou.payer+' '+iou.description+' '+iou.amount+iou.currency+'</li>';
+      historyStr += '<li style="background-color:green">[you received] [&#10003;]'
+        +iou.payer+' '+iou.description+' '+iou.amount+iou.currency
+        +'</li>';
     }
   }
   historyStr += '</ul>';
