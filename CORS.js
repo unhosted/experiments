62a6a3bc53ad2dcb11ae829046ff9a6ef4ea0479
Michiel de Jong <michiel@unhosted.org>
2011-10-29 23:43:52 +0000

    got OPTIONS working in the CORS proxy

--- CORS.js
+++ CORS.js
@@ -21,38 +21,52 @@ http.createServer(function (req, res) {
       , 'path': req.url
       , 'headers': req.headers
       };
-
-    //stop the remote server getting confused and sad, trying to serve a vhost for the proxy's url instead of its own one:
-    options.headers.host = options.host;
-
-    //cunning trick that works because of how our bearer tokens relate to our CouchDb passwords:
-    if(options.headers['Authorization']) {
-      var bearerToken = options.headers['Authorization'].substring(('Bearer '.length));
-      options.headers['Authorization'] = 'Basic '+bearerToken;
-    }
-
-    var req2 = https.request(options, function(res2) {
-      var responseHeaders = res2.headers;
-      console.log('\nC.HEADERS:'+JSON.stringify(responseHeaders));
-      if(false) {
-        responseHeaders['Access-Control-Allow-Origin'] = 'http://example.com';
-        responseHeaders['Access-Control-Allow-Method'] = 'POST';
-        responseHeaders['Access-Control-Allow-Headers'] = 'authorization,content-type,Content-Length,gdata-version,slug,x-upload-content-length,x-upload-content-type';
-        responseHeaders['Access-Control-Allow-Credentials'] = 'true';
+    if(req.method=='OPTIONS') {
+      responseHeaders={}//should maybe get a base set from remote?
+      var origin = req.headers.Origin;
+      if(!origin) {
+        origin = '*';
       }
-      res.writeHead(res2.statusCode, responseHeaders);
-      res2.setEncoding('utf8');
-      var res2Data = '';
-      res2.on('data', function (chunk) {
-        res2Data += chunk;
-        res.write(chunk);
+      responseHeaders['Access-Control-Allow-Origin'] = origin;
+      responseHeaders['Access-Control-Allow-Methods'] = 'GET, PUT, POST, DELETE';
+      responseHeaders['Access-Control-Allow-Headers'] = 'authorization,content-type,Content-Length,gdata-version,slug,x-upload-content-length,x-upload-content-type';
+      responseHeaders['Access-Control-Allow-Credentials'] = 'true';
+      res.writeHead(200, responseHeaders);
+      res.end();
+    } else {
+      //stop the remote server getting confused trying to serve a vhost for the proxy's url instead of its own one:
+      options.headers.host = options.host;
+
+      //cunning trick that works because of how our bearer tokens relate to our CouchDb passwords:
+      if(options.headers['Authorization']) {
+        var bearerToken = options.headers['Authorization'].substring(('Bearer '.length));
+        options.headers['Authorization'] = 'Basic '+bearerToken;
+      }
+
+      console.log('\nB.OPTIONS:'+JSON.stringify(options.headers));
+      var req2 = https.request(options, function(res2) {
+        var responseHeaders = res2.headers;
+        console.log('\nC.HEADERS:'+JSON.stringify(responseHeaders));
+        if(false) {
+          responseHeaders['Access-Control-Allow-Origin'] = 'http://example.com';
+          responseHeaders['Access-Control-Allow-Methods'] = 'GET, PUT, POST, DELETE';
+          responseHeaders['Access-Control-Allow-Headers'] = 'authorization,content-type,Content-Length,gdata-version,slug,x-upload-content-length,x-upload-content-type';
+          responseHeaders['Access-Control-Allow-Credentials'] = 'true';
+        }
+        res.writeHead(res2.statusCode, responseHeaders);
+        res2.setEncoding('utf8');
+        var res2Data = '';
+        res2.on('data', function (chunk) {
+          res2Data += chunk;
+          res.write(chunk);
+        });
+        res2.on('end', function() {
+          console.log('\nC.DATA:'+res2Data);
+          res.end();
+        });
       });
-      res2.on('end', function() {
-        console.log('\nC.DATA:'+res2Data);
-        res.end();
-      });
-    });
-    req2.write(dataStr);
-    req2.end();
+      req2.write(dataStr);
+      req2.end();
+    }
   });
 }).listen(config.proxy.port);
