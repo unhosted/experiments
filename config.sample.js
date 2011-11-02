0f3270a27c798eb6fa7875e61714abe50efacbfb
Michiel de Jong <michiel@unhosted.org>
2011-11-03 00:22:56 +0100

    updating example config

--- config.sample.js
+++ config.sample.js
@@ -1,3 +1,8 @@
+var backends = {
+  statics: 8001,
+  facade: 8002,
+  proxy: 8003
+};
 exports.config = {
   couch: {
     host: '<you>.iriscouch.com',
@@ -5,16 +10,15 @@ exports.config = {
     usr: 'remoteCouch',
     pwd: '<pwd>'
   },
-  proxy: {
-    //host: 'yourremotestorage.com',
-    host: 'myfavouritesandwich.org',
-    port: 9003
-  },
-  facade: {
-    //host: 'yourremotestorage.com',
-    host: 'myfavouritesandwich.org',
-    port: 8003
-  },
+  backends: backends,
   passwords: {
+    'test@example.com': '<pwd>'
+  },
+  defaultPort: backends.statics,
+  proxyHost: 'proxy.example.com',
+  facadeHost: 'example.com',
+  vhosts: {
+    'example.com' : backends.facade,
+    'proxy.example.com' : backends.proxy
   }
 };
