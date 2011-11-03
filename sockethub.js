9f80ac9520fe7a460c5f9bcf6f07eb614a1b7a65
Michiel de Jong <michiel@unhosted.org>
2011-11-03 19:25:08 +0000

    implemented queueing

--- sockethub.js
+++ sockethub.js
@@ -1,15 +1,31 @@
 var socketio = require('socket.io').listen(9000).sockets,
   config = require('./config').config;
 var sockets = {};
+var msgQ = {}
 socketio.on('connection', function(socket) {
   socket.on('register', function(data) {
     if(data.secret == config.socketHubSecret) {
       sockets[data.userAddress] = socket;
       console.log('registered '+data.userAddress);
+      if(msgQ[data.userAddress]) {
+        for(var i in msgQ[data.userAddress]) {
+	  socket.emit('msg', msgQ[data.userAddress][i]);
+	}
+	delete msgQ[data.userAddress];
+      }
     }
   });
   socket.on('msg', function(data) {
     console.log('message for '+data.to);
-    sockets[data.to].emit('msg', data);
+    if(sockets[data.to]) {
+      sockets[data.to].emit('msg', data);
+      console.log('delivered');
+    } else {
+      if(!msgQ[data.to]) {
+        msgQ[data.to]=[];
+      }
+      msgQ[data.to].push(data);
+      console.log('queued');
+    }
   });
 });
