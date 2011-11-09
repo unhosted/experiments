f0d7e58eaa8a172c3a6e4bfa3d93e3e8d0db2a2f
Michiel de Jong <michiel@unhosted.org>
2011-11-09 09:56:44 +0000

    correct 'Logged in as' and not display yourself as a contact

--- domains/opentabs.net/gen4/modules/users.js
+++ domains/opentabs.net/gen4/modules/users.js
@@ -3,11 +3,13 @@ var users = (function(){
     var names = ['Butch', 'Zed', 'Marcellus', 'Jules', 'Mia', 'Fabienne', 'Vincent'];
     var characters = [];
     for(var i in names) {
-      characters.push({
-	      name: names[i],
-    	  userAddress: names[i].toLowerCase()+'@opentabs.net',
-	      avatar:'http://opentabs.net/screens/avatars/'+names[i].toLowerCase()
-	    });
+    	if(localStorage.userAddress != names[i].toLowerCase()+'@opentabs.net') {
+        characters.push({
+	        name: names[i],
+      	  userAddress: names[i].toLowerCase()+'@opentabs.net',
+	        avatar:'http://opentabs.net/screens/avatars/'+names[i].toLowerCase()
+	      });
+      }
     }
     cb(characters);
   }
