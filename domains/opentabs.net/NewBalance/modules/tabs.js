b76e63fa32f15f7ecd750e78d7949b704f5bc4bb
Michiel de Jong <michiel@unhosted.org>
2011-11-10 19:21:53 +0100

    adapt tabs.js to NewBalance

--- domains/opentabs.net/NewBalance/modules/tabs.js
+++ domains/opentabs.net/NewBalance/modules/tabs.js
@@ -8,32 +8,28 @@
 //  > ack
 //
 //data structure for verbs 'propose', 'ack+decline', 'ack+accept', and if iou.diff==0, 'remind':
-//tabs[userAddress][currency] = {
-//  currency: 'USD',
-//  balance: -123,//if i'm borrowing, then balance is negative here.
-//  messages: {//ordered by timestamp, incr
-//    timestamp: {
-//      from: 'user@host',
-//      to: 'user@host',
-//      message: {
-//        verb: 'propose'/'decline'/'accept'/'ack',
-//        tab: {
-//          borrower: 'user@host',
-//          lender: 'user@host',
-//          currency: 'USD',
-//        },
-//        revision: {
-//          timestamp: 1234567890.123,
-//          balance: -123.45
-//        },
-//        previous: 1234567890.123/null,
-//        diff: -123.45
-//        comment: '...'
+//tabs[userAddress][currency] = {//ordered by timestamp, incr
+//  timestamp: {
+//    from: 'user@host',
+//    to: 'user@host',
+//    message: {
+//      verb: 'propose'/'decline'/'accept'/'ack',
+//      tab: {
+//        borrower: 'user@host',
+//        lender: 'user@host',
+//        currency: 'USD',
 //      },
-//      signature: '...' (pgp of utf-8 canonical json of message)
+//      revision: {
+//        timestamp: 1234567890.123,
+//        balance: -123.45
+//      },
+//      previous: 1234567890.123/null,
+//      diff: -123.45
+//      comment: '...'
 //    },
-//    ...
-//  }
+//    signature: '...' (pgp of utf-8 canonical json of message)
+//  },
+//  ...
 //}
 
 var tabs = (function() {
@@ -48,59 +44,25 @@ var tabs = (function() {
       return [];
     }
   }
-  function store(userAddress, iou) {
+  function store(userAddress, message) {
     if(!tabs[userAddress]) {
-      tabs[userAddress] = [];
+      tabs[userAddress] = {};
     }
-    if(!tabs[userAddress][iou.currency]) {
-      tabs[userAddress][iou.currency] = [];
+    if(!tabs[userAddress][message.currency]) {
+      tabs[userAddress][message.currency] = {};
     }
-    tabs[userAddress][iou.currency].push(iou);
+    tabs[userAddress][message.currency][message.revision.timestamp] = message;
     localStorage.setItem('tabs', JSON.stringify(tabs));
   }
   function getTab(userAddress, currency) {
     if(!tabs[userAddress]) {
-      return false;
+      return undefined;
     }
-    for(var i in tabs[userAddress]) {
-      if(tabs[userAddress][i].currency == currency) {
-        return tabs[userAddress][i];
-      }
-    }
-    return false;
-  }
-  function addSignature(userAddress, currency, timestamp, signature) {
-    if(!tabs[userAddress]) {
-      return false;
-    }
-    for(var i in tabs[userAddress]) {
-      if(tabs[userAddress][i].timestamp == timestamp) {
-      if(tabs[userAddress][i].timestamp == timestamp) {
-        tabs[userAddress][i].signatures[localStorage.userAddress] = signature;
-        localStorage.setItem('tabs', JSON.stringify(tabs));
-        return true;
-      }
-    }
-    return false;
-  }
-  function setStatus(userAddress, timestamp, newStatus) {
-    if(!tabs[userAddress]) {
-      return false;
-    }
-    for(var i in tabs[userAddress]) {
-      if(tabs[userAddress][i].timestamp == timestamp) {
-        tabs[userAddress][i].status = newStatus;
-        localStorage.setItem('tabs', JSON.stringify(tabs));
-        return true;
-      }
-    }
-    return false;
+    return tabs[userAddress][currency];
   }
   return {
     getTabs: getTabs,
     getTab: getTab,
-    store: store,
-    setStatus: setStatus,
-    addSignature: addSignature
+    store: store
   }
 })();
