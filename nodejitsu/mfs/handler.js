da4c7a00aac8cfad558f2bf30e421aeff40fda3d
Michiel de Jong <michiel@unhosted.org>
2011-11-29 01:48:15 +0700

    updates to mfs nodejitsu app

---
+++ nodejitsu/mfs/handler.js
@@ -0,0 +1,72 @@
+exports.handler = (function() {
+  var http = require('http'),
+    url = require('url'),
+    path = require('path'),
+    fs = require('fs'),
+    config = require('./config').config;
+   
+  function serve(req, res, staticsMap) {
+    var uripath = url.parse(req.url).pathname
+      .replace(new RegExp('/$', 'g'), '/index.html');
+    var host = req.headers.host;
+    if(config.redirect && config.redirect[host]) {
+      res.writeHead(302, {'Location': 'http://'+config.redirect[host]});
+      res.write('302 Location: http://'+config.redirect[host]+'\n');
+      res.end();
+      return;
+    }
+    console.log(host);
+    console.log(req.url);
+    console.log(uripath);
+    var filename;
+    if(config.path && config.path[host + uripath]) {
+      filename = config.path[host + uripath];
+    } else if(config.host && config.host[host]) {
+      filename = config.host[host] + uripath;
+    } else {
+      filename = config.default + uripath;
+    }
+    var contentType;
+    if(/\.appcache$/g.test(uripath)) {
+      contentType='text/cache-manifest';
+    } else if(/\.html$/g.test(uripath)) {
+      contentType='text/html';
+    } else if(/\.css$/g.test(uripath)) {
+      contentType='text/css';
+    } else if(/\.js$/g.test(uripath)) {
+      contentType='text/javascript';
+    } else if(/\.png$/g.test(uripath)) {
+      contentType='image/png';
+    } else {
+      contentType='text/plain';
+    }
+    path.exists(filename, function(exists) { 
+      if(!exists) { 
+        res.writeHead(404, {'Content-Type': 'text/plain'});
+        res.write('404 Not Found\n'+filename);
+        res.end();
+        return;
+      } 
+   
+      fs.readFile(filename, 'binary', function(err, file) {
+        if(err) {
+          res.writeHead(500, {'Content-Type': 'text/plain'});
+          res.end(err + '\n');
+          return;
+        }
+
+        res.writeHead(200, {
+          'Access-Control-Allow-Origin': '*',
+          'Access-Control-Allow-Headers': 'Content-Type',
+          'Content-Type': contentType
+        });
+        res.write(file, 'binary');
+        res.end();
+      });
+    })
+  }
+
+  return {
+    serve: serve
+  };
+})();
