44a54bb22e5123e043cf31e29a826ed8ab628a31
Michiel de Jong <michiel@unhosted.org>
2011-12-04 09:11:43 +0700

    readding https to mfs

--- nodejitsu/mfs/server.js
+++ nodejitsu/mfs/server.js
@@ -1,4 +1,6 @@
 var http = require('http'),
+  https = require('https'),
+  fs = require('fs'),
   handler = require('./handler').handler,
   config = require('./config').config;
  
@@ -6,4 +8,9 @@ function serve(req, res) {
   return handler.serve(req, res, config.staticsMap);
 }
 http.createServer(serve).listen(config.port);
-console.log('Server running at ports '+config.port); 
+https.createServer({
+  ca:fs.readFileSync(config.sslDir +'sub.class1.server.ca.pem'),
+  key:fs.readFileSync(config.sslDir +'ssl.key'),
+  cert:fs.readFileSync(config.sslDir +'ssl.crt')
+}, serve).listen(config.sslPort);
+console.log('Server running at ports '+config.port+' and '+config.sslPort); 
