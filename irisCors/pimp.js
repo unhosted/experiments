68eb7867d11a8cb505a928b3ee074ce8aeeafb1b
Michiel de Jong <michiel@unhosted.org>
2011-12-13 15:32:12 +0700

    correct quote-escaping in host-meta

--- irisCors/pimp.js
+++ irisCors/pimp.js
@@ -45,32 +45,32 @@ var pimper = (function() {
       "shows": {
         "host-meta": 
           "function(doc, req) { return {"+
-            " body: \""+
+            " \"body\": \""+
             "      <?xml version='1.0' encoding='UTF-8'?>"+
             "      <XRD xmlns='http://docs.oasis-open.org/ns/xri/xrd-1.0' xmlns:hm='http://host-meta.net/xrd/1.0'>"+
-            "        <hm:Host xmlns='http://host-meta.net/xrd/1.0'>michielbdejong.iriscouch.com</hm:Host>"+
+            "        <hm:Host xmlns='http://host-meta.net/xrd/1.0'>"+couchAddress+"</hm:Host>"+
             "        <Link rel='lrdd' template='http://"+couchAddress+"/cors/_design/well-known/_show/webfinger?q={uri}'/>"+
             "      </XRD>\","+
-            "headers: {'Access-Control-Allow-Origin': '*'}"+
+            "\"headers\": {\"Access-Control-Allow-Origin\": \"*\"}"+
           "};}",
         "webfinger": 
           "function(doc, req) { return {"+
             " body: \""+
-            "    <?xml version='1.0' encoding='UTF-8'?>"+
-            "    <XRD xmlns='http://docs.oasis-open.org/ns/xri/xrd-1.0' xmlns:hm='http://host-meta.net/xrd/1.0'>"+
-            "      <hm:Host xmlns='http://host-meta.net/xrd/1.0'>"+couchAddress+"</hm:Host>"+
+            "    <?xml version=\"1.0\" encoding=\"UTF-8\"?>"+
+            "    <XRD xmlns=\"http://docs.oasis-open.org/ns/xri/xrd-1.0\" xmlns:hm=\"http://host-meta.net/xrd/1.0\">"+
+            "      <hm:Host xmlns=\"http://host-meta.net/xrd/1.0\">"+couchAddress+"</hm:Host>"+
             "      <Link"+
-            "        rel='remoteStorage'"+
-            "        api='CouchDB'"+
-            "        auth='http://"+couchAddress+"/cors/auth/modal.html'"+
-            "        template='http://michielbdejong.iriscouch.com/cors/_design/proxy/{uri}/'"+
+            "        rel=\"remoteStorage\""+
+            "        api=\"CouchDB\""+
+            "        auth=\"http://"+couchAddress+"/cors/auth/modal.html\""+
+            "        template=\"http://michielbdejong.iriscouch.com/cors/_design/proxy/{uri}/\""+
             "      />"+
             "    </XRD>\","+
-            "headers: {'Access-Control-Allow-Origin': '*'}"+
+            "headers: {\"Access-Control-Allow-Origin\": \"*\"}"+
           "};}",
         "vep":
           " function(doc, req) { return { body: \"(coming soon)\","+
-            "headers: {'Access-Control-Allow-Origin': '*'}"+
+            "headers: {\"Access-Control-Allow-Origin\": \"*\"}"+
           "};}"
       }
     });
