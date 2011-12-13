c0a5d024e3026cf30241d825c7af484aa3dec00c
Michiel de Jong <michiel@unhosted.org>
2011-12-13 16:30:34 +0700

    wrapping xml inside json inside javascript inside json inside http inside bash inside javascript

--- irisCors/pimp.js
+++ irisCors/pimp.js
@@ -3,7 +3,7 @@ var pimper = (function() {
   function httpPut(address, value, withCredentials, attachment, contentType) {
     var options=' -X PUT';
     if(value) {
-      options += ' -d\''+JSON.stringify(value)+'\'';
+      options += " -d'"+value+"'";
     }
     if(attachment) {
       options += ' --data-urlencode '+attachment;
@@ -19,7 +19,7 @@ var pimper = (function() {
     console.log('curl '+options+' '+host); 
   }
   function createAdminUser() {
-    httpPut(couchAddress+'/_config/admins/'+adminUsr, adminPwd);
+    httpPut(couchAddress+'/_config/admins/'+adminUsr, '\"'+adminPwd+'\"');
   }
   function createDatabase(dbName) {
     httpPut(couchAddress+'/'+dbName, null, true);
@@ -40,40 +40,39 @@ var pimper = (function() {
     adminPwd = password;
     createAdminUser();
     createDatabase('cors');
-    createDocument('cors', '_design/well-known', {
-      "_id":"_design/well-known",
-      "shows": {
-        "host-meta": 
-          "function(doc, req) { return {"+
-            " \"body\": \""+
-            "      <?xml version='1.0' encoding='UTF-8'?>"+
-            "      <XRD xmlns='http://docs.oasis-open.org/ns/xri/xrd-1.0' xmlns:hm='http://host-meta.net/xrd/1.0'>"+
-            "        <hm:Host xmlns='http://host-meta.net/xrd/1.0'>"+couchAddress+"</hm:Host>"+
-            "        <Link rel='lrdd' template='http://"+couchAddress+"/cors/_design/well-known/_show/webfinger?q={uri}'/>"+
-            "      </XRD>\","+
-            "\"headers\": {\"Access-Control-Allow-Origin\": \"*\"}"+
-          "};}",
-        "webfinger": 
-          "function(doc, req) { return {"+
-            " body: \""+
-            "    <?xml version=\"1.0\" encoding=\"UTF-8\"?>"+
-            "    <XRD xmlns=\"http://docs.oasis-open.org/ns/xri/xrd-1.0\" xmlns:hm=\"http://host-meta.net/xrd/1.0\">"+
-            "      <hm:Host xmlns=\"http://host-meta.net/xrd/1.0\">"+couchAddress+"</hm:Host>"+
-            "      <Link"+
-            "        rel=\"remoteStorage\""+
-            "        api=\"CouchDB\""+
-            "        auth=\"http://"+couchAddress+"/cors/auth/modal.html\""+
-            "        template=\"http://michielbdejong.iriscouch.com/cors/_design/proxy/{uri}/\""+
-            "      />"+
-            "    </XRD>\","+
-            "headers: {\"Access-Control-Allow-Origin\": \"*\"}"+
-          "};}",
-        "vep":
-          " function(doc, req) { return { body: \"(coming soon)\","+
-            "headers: {\"Access-Control-Allow-Origin\": \"*\"}"+
-          "};}"
-      }
-    });
+    createDocument('cors', '_design/well-known', '{'+
+      '\"_id\": \"_design/well-known\",'+
+      '\"shows\": {'+
+        '\"host-meta\":'+ 
+          '\"function\(doc, req\) { return {'+
+            ' \\"body\\": \\"'+
+//            '      <?xml version=\"1.0\\" encoding=\\\"UTF-8\\\\"?>'+
+//            '      <XRD xmlns="http://docs.oasis-open.org/ns/xri/xrd-1.0" xmlns:hm="http://host-meta.net/xrd/1.0">'+
+//            '        <hm:Host xmlns=\'http://host-meta.net/xrd/1.0\'>'+couchAddress+'</hm:Host>'+
+//            '        <Link rel=\'lrdd\' template=\'http://'+couchAddress+'/cors/_design/well-known/_show/webfinger?q={uri}\'/>'+
+            '      </XRD>\\",'+
+            '\\"headers\\": {\\"Access-Control-Allow-Origin\\": \\"*\\"}'+
+//          '\"};}\",'+
+//        '\"webfinger\":'+ 
+//          '\"function\(doc, req\) { return {\"'+
+//            " \"body\": \""+
+//            "    <?xml version='1.0' encoding='UTF-8'?>"+
+//            "    <XRD xmlns='http://docs.oasis-open.org/ns/xri/xrd-1.0' xmlns:hm='http://host-meta.net/xrd/1.0'>"+
+//            "      <hm:Host xmlns='http://host-meta.net/xrd/1.0'>"+couchAddress+"</hm:Host>"+
+//            "      <Link"+
+//            "        rel='remoteStorage'"+
+//            "        api='CouchDB'"+
+//            "        auth='http://"+couchAddress+"/cors/auth/modal.html'"+
+//            "        template='http://michielbdejong.iriscouch.com/cors/_design/proxy/{uri}/'"+
+//            "      />"+
+//            "    </XRD>\","+
+//            " \"headers\": {\"Access-Control-Allow-Origin\": \"*\"}"+
+//          '\"};}\",'+
+//        '\"vep":'+
+//          '\" function\(doc, req\) { return { \"body\": \"\(coming soon\)\",'+
+//            "\"headers\": {\"Access-Control-Allow-Origin\": \"*\"}"+
+         '};}\"'+
+         '}}');
     uploadAttachment('cors', 'auth', 'modal.html', 'files/modal.html', 'text/html');
     uploadAttachment('cors', 'base64', 'base64.js', 'files/base64.js', 'application/javascript');
     uploadAttachment('cors', 'sha1', 'sha1.js', 'files/sha1.js', 'application/javascript');
