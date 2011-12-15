cd1e60556110dc04c7ef63405d4d6747b64fa46b
Michiel de Jong <michiel@unhosted.org>
2011-12-16 01:48:29 +0700

    still can't get menu options to show up

--- wp-plugin/remoteStorage/remoteStorage.php
+++ wp-plugin/remoteStorage/remoteStorage.php
@@ -24,10 +24,39 @@ class remoteStoragePlugin {
       wp_die( __('You do not have sufficient permissions to access this page.') );
     }
     echo '<div class="wrap"><form method="post" action="options.php">';
+    settings_fields('remoteStorage');
+    do_settings_sections('remoteStorage');
+    echo '<p class="submit"><input type="submit" class="button-primary" value="<?php _e(\'Save Changes\') ?>" /></p>';
     echo '</form></div>';
+  }
+  function showPluginOptionsText() {
+    echo "I wonder where this text will show up";
+  }
+  function validateOptions() {
+    return true;
+  }
+  function templateField() {
+    $options = get_option('remoteStorage');
+    echo "<input id='remoteStorageTemplate' name='remoteStorage[template]' size='40' type='text' value='{$options['template']}' />";
+  }
+  function authField() {
+    $options = get_option('remoteStorage');
+    echo "<input id='remoteStorageAuth' name='remoteStorage[auth]' size='40' type='text' value='{$options['auth']}' />";
+  }
+  function apiField() {
+    $options = get_option('remoteStorage');
+    echo "<input id='remoteStorageApi' name='remoteStorage[api]' size='40' type='text' value='{$options['api']}' />";
+  }
+  function onAdminInit() {
+    register_setting( 'remoteStorage', 'xrdLinkAttr', array('remoteStoragePlugin', 'validateOptions'));
+    add_settings_section('remoteStorage_main', 'Your remoteStorage details', array('remoteStoragePlugin', 'showPluginOptionsText'), 'remoteStorage');
+    add_settings_field('remoteStorageAttrTemplate', 'template', array('remoteStoragePlugin', 'templateField'), 'remotestorage', 'remoteStorage_main');
+    add_settings_field('remoteStorageAttrAuth', 'auth', array('remoteStoragePlugin', 'authField'), 'remotestorage', 'remoteStorage_main');
+    add_settings_field('remoteStorageAttrApi', 'api', array('remoteStoragePlugin', 'apiField'), 'remotestorage', 'remoteStorage_main');
   }
 }
 
 add_action("webfinger_xrd", array('remoteStoragePlugin', 'onWebfingerXrd'));
 add_action('admin_menu', array('remoteStoragePlugin', 'onAdminMenu'));
+add_action('admin_init', array('remoteStoragePlugin', 'onAdminInit'));
 
