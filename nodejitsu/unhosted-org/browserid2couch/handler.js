1a99fb192c7f3c6633ef83d2e600730bbf05981f
Michiel de Jong <michiel@unhosted.org>
2011-12-23 10:43:20 +0700

    faking browseridaccess flow

--- nodejitsu/unhosted-org/browserid2couch/handler.js
+++ nodejitsu/unhosted-org/browserid2couch/handler.js
@@ -1,9 +1,25 @@
 exports.handler = (function() {
   var url = require('url'),
     querystring = require('querystring'),
-    browseridVerify = require('browserid-verifier');
-   
+    browseridVerify = require('browserid-verifier'),
+    couchConfig = require('./config').config;
+
+  function couchPut(dbName, key, value) {
+    console.log('couchPut("'+dbName+'", "'+key+'", "'+value+'");');
+  }
+  function getBearerToken(userName, dbName, cb) {
+    var userPass = 'asdf';
+    var userObj = {
+      name: userName,
+      password_sha1: 'qwer'
+    }
+    couchPut('_users', 'org.couchdb.user::'+userName, userObj);
+    couchPut(dbName, '', '');
+    var token = 'base64';//(userName.':'.userPass);
+    cb(token);
+  }
   function serve(req, res, baseDir) {
+    console.log('serving browserid2couch');
     var dataStr = '';
     req.on('data', function(chunk) {
       dataStr += chunk;
@@ -15,14 +31,23 @@ exports.handler = (function() {
       //}
       browseridVerify(postData, function(err, r) {
         if(err) {
-          res.writeHead(200, {'Content-type': 'application/json'});
+          console.log('err, allowing origin '+req.headers.origin);
+          console.log(req.headers);
+          res.writeHead(200, {'Content-type': 'application/json', 'Access-Control-Allow-Origin': req.headers.origin});
           res.write(JSON.stringify(err));
           res.end();
         } else {
-          res.writeHead(200, {'Content-type': 'application/json'});
           if(r.email == 'michiel@unhosted.org') {
-            res.write('some-bearer-token');
+            getBearerToken(postData.audience, postData.audience, function(token) {
+              console.log('token, allowing origin '+req.headers.origin);
+              console.log(req.headers);
+              res.writeHead(200, {'Content-type': 'application/json', 'Access-Control-Allow-Origin': req.headers.origin});
+              res.write(token);
+            });
           } else {
+            console.log('nope, allowing origin '+req.headers.origin);
+            console.log(req.headers);
+            res.writeHead(200, {'Content-type': 'application/json', 'Access-Control-Allow-Origin': req.headers.origin});
             res.write('nope');
           }
           res.end();
