c6f573ffecf40425ceb884af860f40144268cc1b
Michiel de Jong <michiel@unhosted.org>
2011-12-29 10:12:14 +0700

    add redirects for libredocs

--- IrisCouchEnrollment/config.js
+++ IrisCouchEnrollment/config.js
@@ -1,5 +1,10 @@
 exports.config = {
   port: 80,
+  redirect: {
+    'www.libredocs.org': 'http://libredocs.org',
+    'www.libredoc.org': 'http://libredocs.org',
+    'libredoc.org': 'http://libredocs.org'
+  },
   host: {
     'libredocs.org': '/pimper'
   },
