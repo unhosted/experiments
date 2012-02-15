a84ac5afbaa3257146972ae730ea77c813a78f38
Michiel de Jong <michiel@unhosted.org>
2012-02-15 17:27:19 +0100

    testPredis

---
+++ surver/php/config.sample.php
@@ -0,0 +1,6 @@
+<?php
+$config = array(
+  'redisHost' => 'shark.redistogo.com',
+  'redisPort' => '1234',
+  'redisPwd' => 'yo-ho'
+);
--- surver/php/testPredis.php
+++ surver/php/testPredis.php
@@ -1,2 +1,13 @@
 <?php
 
+require('./predis/lib/Predis/Autoloader.php');
+require('config.php');
+Predis\Autoloader::register();
+$redis = new Predis\Client(array(
+  'scheme' => 'tcp',
+  'host' => $config['redisHost'],
+  'port' => $config['redisPort']
+  )
+);
+$redis->set('from', 'predizzz');
+echo $redis->get('from');
