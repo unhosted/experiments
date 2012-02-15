d1311af757a2c638246064c6083ca186e7582cea
Michiel de Jong <michiel@unhosted.org>
2012-02-15 12:25:05 +0100

    use req from fkooman

--- surver/saml.js
+++ surver/saml.js
@@ -4,24 +4,21 @@ exports.saml = (function() {
     redis = require('redis'),
     redisClient;
   function getAuthReqUrl() {
-    var id='',
-      issueInstant = '',
-      assertionConsumerServiceUrl = '',
-      issue = '',
-      nameIdentifierFormat = '',
-      idpSsoTargetUrl = '';
-    var request = '<samlp:AuthnRequest xmlns:samlp=\"urn:oasis:names:tc:SAML:2.0:protocol\" ID=\"'+id
-      +'\" Version=\"2.0\" IssueInstant=\"'+issueInstant
-      +'\" ProtocolBinding=\"urn:oasis:names:tc:SAML:2.0:bindings:HTTP-POST\" AssertionConsumerServiceURL=\"'+assertionConsumerServiceUrl
-      +'\">'
-      +'<saml:Issuer xmlns:saml=\"urn:oasis:names:tc:SAML:2.0:assertion\">'+issuer
-      +'</saml:Issuer>\n'
-      +'<samlp:NameIDPolicy xmlns:samlp=\"urn:oasis:names:tc:SAML:2.0:protocol\" Format=\"'+nameIdentifierFormat
-      +'\" AllowCreate=\"true\"></samlp:NameIDPolicy>\n'
-      +'<samlp:RequestedAuthnContext xmlns:samlp=\"urn:oasis:names:tc:SAML:2.0:protocol\" Comparison=\"exact\">'
-      +'<saml:AuthnContextClassRef xmlns:saml=\"urn:oasis:names:tc:SAML:2.0:assertion\">'
-      +'urn:oasis:names:tc:SAML:2.0:ac:classes:PasswordProtectedTransport</saml:AuthnContextClassRef></samlp:RequestedAuthnContext>\n'
-      +'</samlp:AuthnRequest>';
+    var request =
+      '<samlp:AuthnRequest xmlns:samlp="urn:oasis:names:tc:SAML:2.0:protocol"\n'
+      '                    xmlns:saml="urn:oasis:names:tc:SAML:2.0:assertion"\n'
+      '                    ID="_8458820a428d12d12d7dded7418ee109d28a4dd9b8"\n'
+      '                    Version="2.0"\n'
+      '                    IssueInstant="2012-02-15T10:59:40Z"\n'
+      '                    Destination="https://frkosp.wind.surfnet.nl/sspidp/saml2/idp/SSOService.php"\n'
+      '                    AssertionConsumerServiceURL="https://frkosp.wind.surfnet.nl/sspsp/module.php/saml/sp/saml2-acs.php/default-sp"\n'
+      '                    ProtocolBinding="urn:oasis:names:tc:SAML:2.0:bindings:HTTP-POST"\n'
+      '                    >\n'
+      '    <saml:Issuer>https://frkosp.wind.surfnet.nl/sspsp/module.php/saml/sp/metadata.php/default-sp</saml:Issuer>\n'
+      '    <samlp:NameIDPolicy Format="urn:oasis:names:tc:SAML:2.0:nameid-format:transient"\n'
+      '                        AllowCreate="true"\n'
+      '                        />\n'
+      '</samlp:AuthnRequest>\n';
 
     var deflatedRequest = zlib.deflate(new Buffer(request, 'binary')); // also another 'Buffer'
     var base64Request = deflatedRequest.toString('base64');
