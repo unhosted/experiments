16faf75d38cedfb6a90573dcf51461af952fe0cc
Michiel de Jong <michiel@unhosted.org>
2012-02-29 14:25:17 +0000

    syncer.getUserAddress()

--- appsapp/unhosted/syncer.js
+++ appsapp/unhosted/syncer.js
@@ -8,13 +8,14 @@ var syncer = (function() {
       }
       online = true;
       ready = true;
-      readyStateChangeHandler(connected, online, ready);
+      //readyStateChangeHandler(connected, online, ready);
     },
     readyStateChangeHandler = function() {},
     connected = false,
     online = false,
     ready = false,
     connect = function(userAddress, categories, cb) {
+      localStorage['_unhosted$userAddress'] = userAddress;
       var libPath = '/unhosted';
       window.open(libPath+'/openDialog.html'
         +'?userAddress='+encodeURIComponent(userAddress)
@@ -34,15 +35,14 @@ var syncer = (function() {
       localStorage['_unhosted$categoriesToSync'] = JSON.stringify(categories);
       localStorage['_unhosted$bearerToken'] = bearerToken;
       var libPath = '/unhosted';
-      var storageInfo = JSON.parse(localStorage['_unhosted$storageInfo']);
       connected = true;
       var syncFrame = document.createElement('iframe');
       syncFrame.setAttribute('style', 'border-style:none;width:1px;height:1px;');
       syncFrame.src= location.protocol+'//'+location.host+libPath+'/syncFrame.html'
         +'?api='+encodeURIComponent(storageInfo.api)
         +'&template='+encodeURIComponent(storageInfo.template)
-        +'&categories='+encodeURIComponent(localStorage['_unhosted$categoriesToSync'])
-        +'&token='+encodeURIComponent(localStorage['_unhosted$bearerToken']);
+        +'&categories='+encodeURIComponent(categories)
+        +'&token='+encodeURIComponent(bearerToken);
       document.body.appendChild(syncFrame);
       window.addEventListener('message', function(event) {
         if((event.origin == location.protocol +'//'+ location.host) && (event.data.substring(0, 5) == 'sync:')) {
@@ -56,17 +56,26 @@ var syncer = (function() {
       connected = false;
       readyStateChangeHandler(connected, online, ready);
     };
-  if(localStorage['_unhosted$categoriesToSync']) {
-    try {
-      startSync();
-    } catch(e) {
-      console.log('could not sync categories '+localStorage['_unhosted$categoriesToSync']);
+  function onLoad() {
+    if(localStorage['_unhosted$categoriesToSync']) {
+      try {
+        startSync(JSON.parse(localStorage['_unhosted$storageInfo']), JSON.parse(localStorage['_unhosted$categoriesToSync']), localStorage['_unhosted$bearerToken']);
+      } catch(e) {
+        console.log('could not sync categories '+localStorage['_unhosted$categoriesToSync']);
+      }
+    } else {
+      readyStateChangeHandler(false, true, false);
     }
+  }
+  function getUserAddress() {
+    return localStorage['_unhosted$userAddress'];
   }
   return {
     startSync          : startSync,
     connect            : connect,
     disconnect         : disconnect,
-    onReadyStateChange : onReadyStateChange
+    onReadyStateChange : onReadyStateChange,
+    onLoad             : onLoad,
+    getUserAddress,    : getUserAddress
   };
 })();
