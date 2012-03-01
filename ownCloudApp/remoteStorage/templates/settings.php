61a4a4d5c5de63e48e62c0c39d841cd0d78b91bf
Michiel de Jong <michiel@unhosted.org>
2012-03-01 16:06:19 +0000

    hide elt and not refresh on token revoke

--- ownCloudApp/remoteStorage/templates/settings.php
+++ ownCloudApp/remoteStorage/templates/settings.php
@@ -1,13 +1,19 @@
-<form id="mediaform">
 	<fieldset class="personalblock">
 		<?php echo '<strong>'.$l->t('remoteStorage').'</strong> '.OC_User::getUser().'@'.$_SERVER['SERVER_NAME'].'<br>'; ?>
 		<?php echo '<em>Use this address wherever you see the remoteStorage logo</em>'; ?>
-		<br />Apps that currently have access to your ownCloud:<ul>
+		<br />Apps that currently have access to your ownCloud:
+    <script>
+      function revokeToken(token) {
+        var xhr = new XMLHttpRequest();
+        xhr.open('POST', '/apps/remoteStorage/ajax/revokeToken.php', true);
+        xhr.send(token);
+      }
+    </script>
+    <ul>
 		<?php
 			foreach(OC_remoteStorage::getAllTokens() as $token => $details) {
 				echo '<li>'.$details['appUrl'].': '.$details['categories'].' <input type="submit" value="revoke" onclick="'
-					.'$.post(\'/apps/remoteStorage/ajax/revokeToken.php\', \''.$token.'\');"></li>'."\n";
+					.'revokeToken(\''.$token.'\');this.parentNode.style.display=\'none\';"></li>'."\n";
 			}
 		?></ul>
 	</fieldset>
-</form>
