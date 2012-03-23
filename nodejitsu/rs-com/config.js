ce1e77cc3175f3354fb4bcebe49261dcfade027d
Michiel de Jong <michiel@unhosted.org>
2012-03-23 13:57:44 +0100

    redirect remotestoragejs.com to https://github.com/unhosted/remotestorage.js

--- nodejitsu/rs-com/config.js
+++ nodejitsu/rs-com/config.js
@@ -1,9 +1,12 @@
 exports.config = {
   redirect: {
-    'rs-com.nodejitsu.com': 'http://remotestoragejs.com',
-    'www.remotestoragejs.com': 'http://remotestoragejs.com'
+    //'rs-com.nodejitsu.com': 'http://remotestoragejs.com',
+    //'www.remotestoragejs.com': 'http://remotestoragejs.com',
+    'rs-com.nodejitsu.com': 'https://github.com/unhosted/remotestorage.js',
+    'www.remotestoragejs.com': 'https://github.com/unhosted/remotestorage.js',
+    'remotestoragejs.com': 'https://github.com/unhosted/remotestorage.js'
   },
   host: {
-    'remotestoragejs.com': '/unhosted-tutorial'
+    //'remotestoragejs.com': '/unhosted-tutorial'
   }
 };
